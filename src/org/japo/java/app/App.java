00de28f694de55a724e97aa2db36ec28a3016498
AdriGV <adrian.gimeno.vigo@gmail.com>
2019-11-17 18:10:17 +0100

    :tada: Git initial

--- src/org/japo/java/app/App.java
+++ src/org/japo/java/app/App.java
@@ -1,5 +1,5 @@
 /* 
- * Copyright 2019 CiclojkgfM.
+ * Copyright 2019 AdrianGV  adrian.gimeno.alum@iescamp.es.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
